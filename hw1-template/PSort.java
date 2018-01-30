ff586fcbe6b8bae9e98907251268c72e1a0b098d
alexander-ma <alex.ma.806@gmail.com>
2018-01-29 19:33:16 -0600

    first init

---
+++ hw1-template/PSort.java
@@ -0,0 +1,97 @@
+//UT-EID=am74874
+
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class PSort extends RecursiveTask<Integer>{
+	private int[] array;
+	private int begin;
+	private int end;
+	
+	private PSort(int[] array, int begin, int end) {
+		this.array = array;
+		this.begin = begin;
+		this.end = end;
+	}
+	
+	@Override
+	protected Integer compute() {
+		if(end - begin < 16) {
+			insertSort(array, begin, end);
+			return 0;
+		}
+		else {
+			quickSort(array, begin, end);
+			return 1;
+		}
+	}
+	
+	private void quickSort(int[] array, int begin, int end) {
+		if (edgeCase(array, begin, end)) {
+			return;
+		}
+		
+		int pivot = array[(end - begin) / 2];
+		int index = partition(array, begin, end-1, pivot);
+		quickSort(array, begin, index - 1);
+		quickSort(array, index, end);
+		
+	}
+	
+	private int partition(int[] array, int begin, int end, int pivot) {
+		while (begin < end) {
+			while (array[begin] < pivot) {
+				begin ++;
+			}
+			while (array[end] > pivot) {
+				end --;
+			}
+			
+			if (begin < end) {
+				swap(array, begin, end);
+				begin++;
+				end--;
+			}
+		}
+		return begin;
+	}
+
+	private void insertSort(int[] array, int begin, int end) {
+		int i = begin;
+		while(i < end) {
+			int j = i;
+			while(j > begin && array[j - 1] > array[j]) {
+				//swap values
+				swap(array, j, j-1);
+				j--;
+			}
+			i++;
+		}
+		
+	}
+	
+	private void swap(int[] arr, int begin, int end) {
+		int temp = arr[begin];
+		arr[begin] = array[end];
+		arr[end] = temp;
+	}
+
+	private static ForkJoinPool pool = new ForkJoinPool();
+	
+	public static void parallelSort(int[] A, int begin, int end){
+		if(edgeCase(A, begin, end))
+			return;
+		
+		pool.invoke(new PSort(A, begin, end));
+		
+	}
+	
+	
+	
+	private static boolean edgeCase(int[] arr, int begin, int end) { 
+		if(arr.length == 0 || begin >= end)
+			return true;
+		return false;
+	}
+}
