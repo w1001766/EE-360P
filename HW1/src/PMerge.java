8dc64d357e59e98955ade268934537bfbabf2648
danny-vo <Danny.Vo@utexas.edu>
2018-02-02 23:10:25 -0600

    Started code skeleton for merging

--- HW1/src/PMerge.java
+++ HW1/src/PMerge.java
@@ -1,4 +1,4 @@
-//UT-EID=
+//UT-EID=dpv292
 
 
 import java.util.*;
@@ -6,7 +6,68 @@ import java.util.concurrent.*;
 
 
 public class PMerge{
+  /**
+   * Class that implements callable for parallel execution. Given an element
+   * and the array it is to be merge with, determine its index in the final
+   * array containing the two arrays to be merged.
+   */
+  private class Ranker implements Callable<Integer> {
+    private int element;
+    private int elemArrIdx;
+    private int elemArrSize;
+    private int[] arrToMerge = null;
+
+    /**
+     * Constructor for the Ranker class.
+     * @param element     integer element to determine index of
+     * @param elemArrIdx  the index of the element in its original array
+     * @param elemArrSize the element's original array's size
+     * @param arrToMerge  the array containing the elements to merge with
+     */
+    public Ranker(int element, int elemArrIdx, int elemArrSize, int[] arrToMerge) {
+      this.element = element;
+      this.elemArrIdx = elemArrIdx;
+      this.elemArrSize = elemArrSize;
+      this.arrToMerge = arrToMerge;
+    }
+
+    public Integer call() throws Exception {
+      
+      return -1;
+    }
+
+    private int binarySearch(int elem, int left, int right, int[] arrToMerge) {
+      return -1;
+    }
+  }
+
   public static void parallelMerge(int[] A, int[] B, int[]C, int numThreads){
     // TODO: Implement your parallel merge function
+    final ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+   
+    // Determine A's elements' indices
+    for (int i=0; i<A.length; ++i) {
+      Future<Integer> idx = executorService.submit(new PMerge().
+                                                   new Ranker(A[i],i,A.length,B));
+      try {
+        C[idx.get()] = A[i];
+      } catch (InterruptedException | ExecutionException e) {
+        e.printStackTrace();
+      }
+    }
+
+    // Determine B's elements' indices
+    for (int j=0; j<B.length; ++j) {
+      Future<Integer> idx = executorService.submit(new PMerge().
+                                                   new Ranker(B[j],j,B.length,A));
+      try {
+        C[idx.get()] = B[j];
+      } catch (InterruptedException | ExecutionException e) {
+        e.printStackTrace();
+      }
+    }
+  
+    // Tasks complete
+    executorService.shutdown();
   }
 }
