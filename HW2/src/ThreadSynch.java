5115dbf15d8966522fd7196781d56ecbbcc33a86
alexander-ma <alex.ma.806@gmail.com>
2018-02-12 18:01:36 -0600

    trying different implementation. Works for some cases, but deadlocks frequently.

--- HW2/src/ThreadSynch.java
+++ HW2/src/ThreadSynch.java
@@ -5,69 +5,29 @@
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
 
 public class ThreadSynch {
-  private static final boolean debug = false;
   Semaphore countingSemaphore = null;
-  Semaphore[] threadSemaphores = null;
+  Semaphore threadSemaphore = null;
+  int parties = 0;
 
 	public ThreadSynch(int parties) {
-    // "Gate" semaphore
     this.countingSemaphore = new Semaphore(parties);
-    
-    threadSemaphores = new Semaphore[parties];
-    for (int i = 0; i < parties; ++i) {
-      this.threadSemaphores[i] = new Semaphore(1);
-    }
-
+    this.threadSemaphore = new Semaphore(0);
+    this.parties = parties;
 	}
 	
 	public int await() throws InterruptedException {
-    
-    if (this.countingSemaphore.availablePermits() == 1) {
-      if (ThreadSynch.debug) {
-        System.out.println("CountingSemaphore has 1 permit left");
-      }
-      this.threadSemaphores[0].acquire();
-      this.countingSemaphore.acquire();
-      boolean allAwaited = true;
-      for (int i = 0; i < this.threadSemaphores.length; ++i) {
-          if (ThreadSynch.debug) {
-            System.out.println("Semaphore checking: " + this.threadSemaphores[i].toString());
-          }
-        allAwaited |= this.threadSemaphores[i].availablePermits() == 0;
-      }
-      if (allAwaited) {
-        for (int i = 0; i < this.threadSemaphores.length; ++i) {
-          this.threadSemaphores[i].release();
-          if (ThreadSynch.debug) {
-            System.out.println("Semaphore released: " + this.threadSemaphores[i].toString());
-          }
-        }
-        this.countingSemaphore.release(this.threadSemaphores.length);
-        if (ThreadSynch.debug) {
-          System.out.println("Counting semaphore reset: " + this.countingSemaphore.toString());
-        }
-        return 0;
-      }
-    } else {
-      int threadCount = this.countingSemaphore.availablePermits() - 1;
-      if (threadSemaphores[threadCount].availablePermits() != 0) {
-        if (ThreadSynch.debug) {
-          System.out.println("Semaphore number: " + threadCount);
-          System.out.println("Semaphore acquiring lock: " + this.threadSemaphores[threadCount].toString());
-        }
-        this.threadSemaphores[threadCount].acquire();
-        if (ThreadSynch.debug) {
-          System.out.println("Semaphore number: " + threadCount);
-          System.out.println("Semaphore acquiring lock: " + this.threadSemaphores[threadCount].toString());
-        }
-        countingSemaphore.acquire();
-        if (ThreadSynch.debug) {
-          System.out.println("Counting semaphore: " + this.countingSemaphore.toString());
-        }
-        return threadCount;
-      }
+    // Acquire a permit from the countingSemaphore
+    this.countingSemaphore.acquire();  
+    // Put the threads to bed until last thread arrives
+    if(countingSemaphore.availablePermits() != 0){
+      this.threadSemaphore.acquire();
+      return countingSemaphore.availablePermits() - 1;
     }
 
-    return -1;
-	}
+    else{
+    this.threadSemaphore.release(parties - 1);
+    countingSemaphore.release(parties);
+    return countingSemaphore.availablePermits() - 1;
+    }
+  }
 }
