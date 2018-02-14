5733278ec2e5f9ec995eb2d791e6666292406bdf
danny-vo <dannyphvo@gmail.com>
2018-02-14 17:02:18 -0600

    Added default constructor

--- HW2/src/FairReadWriteLock.java
+++ HW2/src/FairReadWriteLock.java
@@ -1,15 +1,22 @@
 public class FairReadWriteLock {
-  private static int totalRWRequests = 0;
-  private static int RWRequestsServed = 0;
-	private static int activeReaders = 0;
-  private static int activeWriters = 0;
+  private int totalRWRequests;
+  private int rwRequestsServed;
+	private int activeReaders;
+  private int activeWriters;
+
+  public FairReadWriteLock() {
+    this.totalRWRequests = 0;
+    this.rwRequestsServed = 0;
+    this.activeReaders = 0;
+    this.activeWriters = 0;
+  }
 
 	public synchronized void beginRead() {
     // This thread is served after preceeding threads
     int readThreadPriority = totalRWRequests++;
     
     // Wait for preceeding reader threads to be served
-    while (activeWriters > 0 || RWRequestsServed < totalRWRequests) {
+    while (activeWriters > 0 || rwRequestsServed < totalRWRequests) {
       try {
         wait();
       } catch (InterruptedException ie) {
@@ -19,7 +26,7 @@ public class FairReadWriteLock {
     
     // Thread is now reading
     ++activeReaders;
-    ++RWRequestsServed;
+    ++rwRequestsServed;
     notifyAll();
   }
 	
@@ -33,7 +40,7 @@ public class FairReadWriteLock {
 
     // Wait for preceeding reader and writer threads to be served
     while (activeReaders > 0 || activeWriters > 0
-          || RWRequestsServed < writeThreadPriority) {
+          || rwRequestsServed < writeThreadPriority) {
       try {
         wait();
       } catch (InterruptedException ie) {
@@ -43,7 +50,7 @@ public class FairReadWriteLock {
     
     // Thread is now writing
     ++activeWriters;
-    ++RWRequestsServed;
+    ++rwRequestsServed;
     notifyAll();
   }
 	
