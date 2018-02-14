06210d1a9f05a24a9bcf92301be361eec998c0e8
alexander-ma <alex.ma.806@gmail.com>
2018-02-14 01:20:14 -0600

    started #2

--- HW2/src/FairReadWriteLock.java
+++ HW2/src/FairReadWriteLock.java
@@ -1,15 +1,28 @@
 public class FairReadWriteLock {
-	
+	int readers;
+  int writers;
+
+  public FairReadWriteLock() {
+    readers = 0;
+    writers = 0;
+  }
                         
 	public synchronized void beginRead() {
-	}
+	  readers++;
+  }
 	
 	public synchronized void endRead() {
-	}
+	  readers--;
+    notifyAll();
+  }
 	
 	public synchronized void beginWrite() {
-	}
-	public synchronized void endWrite() {
-	}
+	  writers++;
+  }
+	
+  public synchronized void endWrite() {
+	  writers--;
+    notifyAll();
+  }
 }
 	
