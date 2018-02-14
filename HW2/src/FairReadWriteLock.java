8438833489516060ebfd100cae62a94c27a215f2
danny-vo <dannyphvo@gmail.com>
2018-02-14 14:58:50 -0600

    Draft of readwrite lock

--- HW2/src/FairReadWriteLock.java
+++ HW2/src/FairReadWriteLock.java
@@ -1,28 +1,53 @@
 public class FairReadWriteLock {
-	int readers;
-  int writers;
+  private static int totalRWRequests = 0;
+  private static int RWRequestsServed = 0;
+	private static int readers = 0;
+  private static int writers = 0;
 
-  public FairReadWriteLock() {
-    readers = 0;
-    writers = 0;
-  }
-                        
 	public synchronized void beginRead() {
-	  readers++;
+    // This thread is served after preceeding threads
+    int readThreadPriority = totalRWRequests++;
+    
+    // Wait for preceeding reader threads to be served
+    while (readers > 0 || RWRequestsServed < totalRWRequests) {
+      try {
+        wait();
+      } catch (InterruptedException ie) {
+        ie.printStackTrace();
+      }
+    }
+    
+    // Thread is now reading
+    ++readers;
+    ++RWRequestsServed;
+    notifyAll();
   }
 	
 	public synchronized void endRead() {
-	  readers--;
-    notifyAll();
+    if (--readers == 0) notifyAll();
   }
 	
 	public synchronized void beginWrite() {
-	  writers++;
+    // This thread is served after preceeding threads
+    int writeThreadPriority = totalRWRequests++;
+
+    // Wait for preceeding reader and writer threads to be served
+    while (RWRequestsServed < writeThreadPriority || readers > 0 || writers > 0) {
+      try {
+        wait();
+      } catch (InterruptedException ie) {
+        ie.printStackTrace();
+      }
+    }
+    
+    // Thread is now writing
+    ++writers;
+    ++RWRequestsServed;
+    notifyAll();
   }
 	
   public synchronized void endWrite() {
-	  writers--;
-    notifyAll();
+    if (--writers == 0) notifyAll();
   }
 }
 	
