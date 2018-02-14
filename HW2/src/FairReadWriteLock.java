399c1a9c952084309a927cfc9cb1620f912c2b50
alexander-ma <alex.ma.806@gmail.com>
2018-02-13 18:24:43 -0600

    attempted #1 again, to no avail

--- HW2/src/FairReadWriteLock.java
+++ HW2/src/FairReadWriteLock.java
@@ -1,15 +1,27 @@
+import java.util.*;
+
 public class FairReadWriteLock {
-	
+  int readers = 0;
+  int writers = 0;
+  LinkedList<Long> rWait = new LinkedList<>();
+  LinkedList<Long> wWait = new LinkedList<>();
                         
 	public synchronized void beginRead() {
-	}
+    readers++;
+    
+  }
 	
 	public synchronized void endRead() {
-	}
+	  readers--;
+  }
 	
 	public synchronized void beginWrite() {
-	}
-	public synchronized void endWrite() {
-	}
+	  writers++;
+  }
+	
+  public synchronized void endWrite() {
+	  writers--;
+    notifyAll();
+  }
 }
 	
--- HW2/src/ThreadSynch.java
+++ HW2/src/ThreadSynch.java
@@ -7,11 +7,13 @@ import java.util.concurrent.Semaphore; // for implementation using Semaphores
 public class ThreadSynch {
   Semaphore countingSemaphore = null;
   Semaphore threadSemaphore = null;
+  Semaphore releaseSemaphore = null;
   int parties = 0;
 
 	public ThreadSynch(int parties) {
     this.countingSemaphore = new Semaphore(parties);
     this.threadSemaphore = new Semaphore(0);
+    this.releaseSemaphore = new Semaphore(parties);
     this.parties = parties;
 	}
 	
@@ -21,13 +23,17 @@ public class ThreadSynch {
     // Put the threads to bed until last thread arrives
     if(countingSemaphore.availablePermits() != 0){
       this.threadSemaphore.acquire();
+      //TODO: Find a way to let the threads go out at once.
+      //Also, the return values are completely wrong, but i'm not sure what the
+      // values are used for???
       return countingSemaphore.availablePermits() - 1;
     }
 
     else{
-    this.threadSemaphore.release(parties - 1);
-    countingSemaphore.release(parties);
-    return countingSemaphore.availablePermits() - 1;
+      System.out.println("Releasing threads...");
+      countingSemaphore.release(parties);
+      this.threadSemaphore.release(parties - 1);
+      return 0;
     }
   }
 }
