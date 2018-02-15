9d99241ae8f2ebde9fc7cc2c5f28796d0c8296c8
alexander-ma <alex.ma.806@gmail.com>
2018-02-15 00:28:52 -0600

    q3 progress

--- HW2/src/PriorityQueue.java
+++ HW2/src/PriorityQueue.java
@@ -1,20 +1,34 @@
 import java.util.*;
 import java.util.concurrent.*;
 public class PriorityQueue {
-  PriorityQueue <Node> queue;
-  ReentrantLock head;
-  ReentrantLock tail;
-  // Condition full;
-  // Condition empty;
+  
+  class Node {
+      public String data;
+      public int priority;
+      public Node next;
+      public ReentrantLock nodeLock;
+  }
+  
+  ReentrantLock enq;
+  ReentrantLock deq;
+  Condition full;
+  Condition empty;
+  Node head;
+  Node tail;
+  AtomicInteger size;
+  int capacity;
 
 
 	public PriorityQueue(int maxSize) {
     // Creates a Priority queue with maximum allowed size as capacity
-	  queue = new PriorityQueue<>(maxSize);
-    head = new ReentrantLock();
-    tail = new ReentrantLock();
+    enq = new ReentrantLock();
+    deq = new ReentrantLock();
     full = lock.newCondition();
     empty = lock.newCondition();
+    head = new Node(null);
+    tail = head;
+    size = new AtomicInteger(0);
+    capacity = maxSize;
   }
 
 	public int add(String name, int priority) {
@@ -30,13 +44,34 @@ public class PriorityQueue {
 	public int search(String name) {
     // Returns the position of the name in the list;
     // otherwise, returns -1 if the name is not found.
-    return -1;
+    
+    int index = -1;
+    boolean found = false;
+    Node node = queue.peek();
+    
+    while(!found){
+      .lock();
+      if(name.equals(head.data)) found = true;
+      else{
+        index++;
+        node = node.next;
+      }
+      .unlock();
+    }
+
+    if(found) return index;
+    else return -1;
 	}
 
 	public String getFirst() {
-        // Retrieves and removes the name with the highest priority in the list,
-        // or blocks the thread if the list is empty.
-     return "";
+    // Retrieves and removes the name with the highest priority in the list,
+    // or blocks the thread if the list is empty.
+    if(size.get() == 0){
+      //block the thread
+    }
+    
+
+    return "";
 	}
 }
 
