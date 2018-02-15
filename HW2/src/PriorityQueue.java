d60c0e81cea84c5d7732c4627b09e3bd686bf5ca
alexander-ma <alex.ma.806@gmail.com>
2018-02-15 02:47:52 -0600

    finished, need to clean up code

--- HW2/src/PriorityQueue.java
+++ HW2/src/PriorityQueue.java
@@ -1,4 +1,3 @@
-import java.util.*;
 import java.util.concurrent.*;
 public class PriorityQueue {
   
@@ -7,14 +6,29 @@ public class PriorityQueue {
       public int priority;
       public Node next;
       public ReentrantLock nodeLock;
+
+      public Node(){
+        data = null;
+        priority = -1;
+        next = null;
+        nodeLock = null;
+      }
+      
+      public Node(String name, int priority){
+        data = name;
+        priority = priority;
+        next = null;
+        nodeLock = new ReentrantLock();
+      }
+
+
   }
   
   ReentrantLock enq;
   ReentrantLock deq;
   Condition full;
   Condition empty;
-  Node head;
-  Node tail;
+  Node head, tail;
   AtomicInteger size;
   int capacity;
 
@@ -26,7 +40,7 @@ public class PriorityQueue {
     full = lock.newCondition();
     empty = lock.newCondition();
     head = new Node(null);
-    tail = head;
+    tail = head.next;
     size = new AtomicInteger(0);
     capacity = maxSize;
   }
@@ -37,41 +51,137 @@ public class PriorityQueue {
     // otherwise, returns -1 if the name is already present in the list.
     // This method blocks when the list is full.
     
+    boolean wakeDeq = false;
+    enq.lock();
+    try{
+      while(size.get() == capacity){
+        full.await();
+      }
 
-    return -1;
+      Node n = new Node(name, priority);
+      
+
+      //Search if the name exists in the pqueue
+      if(search(name) == -1) return -1;
+
+      //Search for the place to add the new node.
+      boolean found = false;
+      int index = 0;
+      Node first = head;
+      Node second = head.next;
+      
+      first.nodeLock.lock();
+      second.nodeLock.lock();
+      n.nodeLock.lock();
+      while(!found){
+        //Edge case: inserting in an empty queue
+        if(first == null && second == null){
+          first.next = n;
+          n.next = second;
+          found = true;
+        }
+        
+        //Edge case: inserting at beginning of queue
+        if(first == null && (second.priority < n.priority)){
+          first.next = n;
+          n.next = second;
+          found = true;
+        }
+
+        //Edge case: inserting at end of queue
+        if((first.priority > n.priority) && second == null){
+          first.next = n;
+          n.next = second;
+          found = true;
+        }
+        
+        //Found the right place to insert (in the general case)
+        else if((first.priority <= n.priority) && (n.priority < second.priority)){
+          first.next = n;
+          n.next = second;
+          found = true;
+        }
+        
+        //Hand-over-hand method: swap locks and try again
+        else {
+          first.nodeLock.unlock();
+          first = second;
+          second = second.next;
+          index++;
+        }
+      }
+      first.nodeLock.unlock();
+      second.nodeLock.unlock();
+      n.nodeLock.unlock();
+    }
+    finally{
+      return index;
+    }
 	}
 
 	public int search(String name) {
     // Returns the position of the name in the list;
     // otherwise, returns -1 if the name is not found.
     
-    int index = -1;
+    int index = 0;
     boolean found = false;
-    Node node = queue.peek();
+    Node first = head;
+    Node second = head.next;
     
-    while(!found){
-      .lock();
-      if(name.equals(head.data)) found = true;
-      else{
-        index++;
-        node = node.next;
+    first.nodeLock.lock();
+    second.nodeLock.lock();
+    while(!found && first.next == null){
+      if(name.equals(first.data)){
+        found = true;
       }
-      .unlock();
+      else{
+        first.nodeLock.unlock();
+        first = second;
+        second = second.next;
+        index++;
     }
+    first.nodeLock.unlock();
+    second.nodeLock.unlock();
 
-    if(found) return index;
-    else return -1;
-	}
+    return found ? index : -1;
+	  }
+  }
 
 	public String getFirst() {
     // Retrieves and removes the name with the highest priority in the list,
     // or blocks the thread if the list is empty.
-    if(size.get() == 0){
-      //block the thread
-    }
-    
+    String result;
+    boolean wakeEnq = false;
+    deq.lock();
+    try{
+      while(size.get() == 0){
+        //block the thread
+        empty.await();
+      }
+      //dequeue the first node's string
+      result = head.next.data();
+      head = head.next;
 
-    return "";
+      //Set a flag to see if there's space in queue
+      if(size.getAndDecrement() == 0) {
+        wakeEnq = true;
+      }
+    }
+    finally {
+      deq.unlock();
+    }
+
+    //Notify all enqueue threads that there's space
+    if (wakeEnq){
+      enq.lock();
+      try{
+        full.signalAll();
+      }
+      finally{
+        enq.unlock();
+      }
+    }
+    return result;
 	}
 }
 
