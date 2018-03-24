c3c89b82afae4e29fb366c28dc5944e7d89ae031
danny-vo <dannyphvo@gmail.com>
2018-03-24 15:30:03 -0500

    2 space indentions

--- HW4/src/TextAnalyzer.java
+++ HW4/src/TextAnalyzer.java
@@ -13,95 +13,95 @@ import org.apache.hadoop.util.ToolRunner;
 // Do not change the signature of this class
 public class TextAnalyzer extends Configured implements Tool {
 
-    // Replace "?" with your own output key / value types
-    // The four template data types are:
-    //     <Input Key Type, Input Value Type, Output Key Type, Output Value Type>
-    public static class TextMapper extends Mapper<LongWritable, Text, ?, ?> {
-        public void map(LongWritable key, Text value, Context context)
-            throws IOException, InterruptedException
-        {
-            // Implementation of you mapper function
-        }
+  // Replace "?" with your own output key / value types
+  // The four template data types are:
+  //     <Input Key Type, Input Value Type, Output Key Type, Output Value Type>
+  public static class TextMapper extends Mapper<LongWritable, Text, ?, ?> {
+    public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException
+    {
+      // Implementation of you mapper function
     }
+  }
 
-    // Replace "?" with your own key / value types
-    // NOTE: combiner's output key / value types have to be the same as those of mapper
-    public static class TextCombiner extends Reducer<?, ?, ?, ?> {
-        public void reduce(Text key, Iterable<Tuple> tuples, Context context)
-            throws IOException, InterruptedException
-        {
-            // Implementation of you combiner function
-        }
+  // Replace "?" with your own key / value types
+  // NOTE: combiner's output key / value types have to be the same as those of mapper
+  public static class TextCombiner extends Reducer<?, ?, ?, ?> {
+    public void reduce(Text key, Iterable<Tuple> tuples, Context context)
+      throws IOException, InterruptedException
+    {
+      // Implementation of you combiner function
     }
+  }
 
-    // Replace "?" with your own input key / value types, i.e., the output
-    // key / value types of your mapper function
-    public static class TextReducer extends Reducer<?, ?, Text, Text> {
-        private final static Text emptyText = new Text("");
+  // Replace "?" with your own input key / value types, i.e., the output
+  // key / value types of your mapper function
+  public static class TextReducer extends Reducer<?, ?, Text, Text> {
+    private final static Text emptyText = new Text("");
 
-        public void reduce(Text key, Iterable<Tuple> queryTuples, Context context)
-            throws IOException, InterruptedException
-        {
-            // Implementation of you reducer function
+    public void reduce(Text key, Iterable<Tuple> queryTuples, Context context)
+      throws IOException, InterruptedException
+    {
+      // Implementation of you reducer function
 
-            // Write out the results; you may change the following example
-            // code to fit with your reducer function.
-            //   Write out the current context key
-            context.write(key, emptyText);
-            //   Write out query words and their count
-            for(String queryWord: map.keySet()){
-                String count = map.get(queryWord).toString() + ">";
-                queryWordText.set("<" + queryWord + ",");
-                context.write(queryWordText, new Text(count));
-            }
-            //   Empty line for ending the current context key
-            context.write(emptyText, emptyText);
-        }
+      // Write out the results; you may change the following example
+      // code to fit with your reducer function.
+      //   Write out the current context key
+      context.write(key, emptyText);
+      //   Write out query words and their count
+      for(String queryWord: map.keySet()){
+        String count = map.get(queryWord).toString() + ">";
+        queryWordText.set("<" + queryWord + ",");
+        context.write(queryWordText, new Text(count));
+      }
+      //   Empty line for ending the current context key
+      context.write(emptyText, emptyText);
     }
+  }
 
-    public int run(String[] args) throws Exception {
-        Configuration conf = this.getConf();
+  public int run(String[] args) throws Exception {
+    Configuration conf = this.getConf();
 
-        // Create job
-        Job job = new Job(conf, "EID1_EID2"); // Replace with your EIDs
-        job.setJarByClass(TextAnalyzer.class);
+    // Create job
+    Job job = new Job(conf, "EID1_EID2"); // Replace with your EIDs
+    job.setJarByClass(TextAnalyzer.class);
 
-        // Setup MapReduce job
-        job.setMapperClass(TextMapper.class);
-        //   Uncomment the following line if you want to use Combiner class
-        // job.setCombinerClass(TextCombiner.class);
-        job.setReducerClass(TextReducer.class);
+    // Setup MapReduce job
+    job.setMapperClass(TextMapper.class);
+    //   Uncomment the following line if you want to use Combiner class
+    // job.setCombinerClass(TextCombiner.class);
+    job.setReducerClass(TextReducer.class);
 
-        // Specify key / value types (Don't change them for the purpose of this assignment)
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        //   If your mapper and combiner's  output types are different from Text.class,
-        //   then uncomment the following lines to specify the data types.
-        //job.setMapOutputKeyClass(?.class);
-        //job.setMapOutputValueClass(?.class);
+    // Specify key / value types (Don't change them for the purpose of this assignment)
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    //   If your mapper and combiner's  output types are different from Text.class,
+    //   then uncomment the following lines to specify the data types.
+    //job.setMapOutputKeyClass(?.class);
+    //job.setMapOutputValueClass(?.class);
 
-        // Input
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        job.setInputFormatClass(TextInputFormat.class);
+    // Input
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    job.setInputFormatClass(TextInputFormat.class);
 
-        // Output
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setOutputFormatClass(TextOutputFormat.class);
+    // Output
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.setOutputFormatClass(TextOutputFormat.class);
 
-        // Execute job and return status
-        return job.waitForCompletion(true) ? 0 : 1;
-    }
+    // Execute job and return status
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
 
-    // Do not modify the main method
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new TextAnalyzer(), args);
-        System.exit(res);
-    }
+  // Do not modify the main method
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new Configuration(), new TextAnalyzer(), args);
+    System.exit(res);
+  }
 
-    // You may define sub-classes here. Example:
-    // public static class MyClass {
-    //
-    // }
+  // You may define sub-classes here. Example:
+  // public static class MyClass {
+  //
+  // }
 }
 
 
