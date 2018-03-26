257fdfc6d841ca28a1aeba14e69d1bdff58170ed
alexander-ma <alex.ma.806@gmail.com>
2018-03-25 19:57:48 -0500

    I think we finished, need to test on AWS

--- HW4/src/TextAnalyzer.java
+++ HW4/src/TextAnalyzer.java
@@ -12,79 +12,147 @@ import org.apache.hadoop.util.ToolRunner;
 
 // Import everything for now...
 import java.util.*;
-import hava.io.*;
+import java.io.*;
 
 // Do not change the signature of this class
 public class TextAnalyzer extends Configured implements Tool {
 
-/*---------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------*/
 	// TEXT MAPPER
     // Replace "?" with your own output key / value types
     // The four template data types are:
     //     <Input Key Type, Input Value Type, Output Key Type, Output Value Type>
 
-    public static class TextMapper extends Mapper<LongWritable, Text, ?, ?> {
-        public void map(LongWritable key, Text value, Context context)
-            throws IOException, InterruptedException
-        {
-            // Implementation of you mapper function
+	public static class TextMapper extends Mapper<LongWritable, Text, Text, MapWritable> {
+		public void map(LongWritable key, Text value, Context context)
+		throws IOException, InterruptedException
+		{
+        // Implementation of your mapper function
 
-            // Read line and format accordingly (lowercase, non-word character string)
-            // replaceAll("[^\\p{L}\\p{Nd}]+", "") also works as well in case of UTF8 
-            String line = value.toString().toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", " ");
-            StringTokenizer itr = new StringTokenizer(line);
-	        while (itr.hasMoreTokens()) {
-	         	word.set(itr.nextToken());
-	        	context.write(word, one);
-	      	}
-        }
-    }
+        // Read line and format accordingly (lowercase, non-word character string)
+        // replaceAll("[^\\p{L}\\p{Nd}]+", "") also works as well in case of UTF8 
+			private String line = value.toString().toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", " ");
+			private String[] words = line.split(" ");
 
-/*---------------------------------------------------------------------------*/
+        // Create a set of all words and # of occurrences in line 
+			private TreeMap<String, IntWritable> wordSet = new TreeMap<>();
+			for(String word: words){
+				//Text word_txt = new Text(word);
+				if(!wordSet.containsKey(word))
+					wordSet.put(word, new IntWritable(1));
+				else
+					wordSet.put(word, wordSet.get(word)+1);
+			}
+
+      // Send key-value pairs <Text, MapWritable<Text, IntWritable>> to Combiner/Reducer
+			for(String word : wordSet.keySet()){
+				Text contextword = new Text(word);
+
+        // Create MapWritables containing key-value pairs <queryword, occurrence>
+				MapWritable wordmap = new MapWritable();
+
+        // Remove a single occurence of that word from the wordset
+        int count = wordSet.get(word)-1;
+        if(count != 0)
+					wordSet.put(word, wordSet.get(word)-1);
+				else
+					wordSet.remove(word);
+
+        // Write to MapWritable
+				for(Map.Entry<String, IntWritable> entry: wordSet.entrySet())
+					wordmap.put(new Text(entry.getKey()), entry.getValue());
+
+        // Send out contextword, querywords<occurrences>
+				context.write(contextword, wordmap);
+
+        // Add the occurence back into the wordset
+        if(count != 0)
+					wordSet.put(word, wordSet.get(word)+1);
+				else
+					wordSet.put(word, new IntWritable(1));
+			}
+		}
+	}
+
+	/*---------------------------------------------------------------------------*/
 	// TEXT COMBINER
     // Replace "?" with your own key / value types
     // NOTE: combiner's output key / value types have to be the same as those of mapper
 
-    public static class TextCombiner extends Reducer<?, ?, ?, ?> {
-        public void reduce(Text key, Iterable<Tuple> tuples, Context context)
-            throws IOException, InterruptedException
-        {
-            // Implementation of you combiner function
-        }
-    }
+	public static class TextCombiner extends Reducer<Text, MapWritable, Text, MapWritable> {
+		public void reduce(Text key, Iterable<MapWritable> wordmaps, Context context)
+		throws IOException, InterruptedException
+		{
+      // Implementation of your combiner function
+			private TreeMap<String, IntWritable> wordSet = new TreeMap<>();
 
-/*---------------------------------------------------------------------------*/
+      // Combine querywords-occurrence pairs into one TreeMap
+      for(MapWritable wordmap : wordmaps){
+      	for(Map.Entry<Text, IntWritable> entry: wordmap.entrySet()){
+					String query = entry.getKey().toString();
+					IntWritable count = entry.getValue();
+					if(wordSet.containsKey(query)){
+						wordSet.getValue(query).set(wordSet.getValue(query).get() + count.get());
+					}
+					else{
+						wordSet.put(query, count);
+					}
+      	}
+			}
+
+			// Create new MapWritable to send to the reducer
+			MapWritable wordmap = new MapWritable();
+
+			// Write to MapWritable
+			for(Map.Entry<String, IntWritable> entry: wordSet.entrySet())
+				wordmap.put(new Text(entry.getKey()), entry.getValue());
+
+			// Send it off to the reducer to be outputted
+			context.write(key, wordmap);
+		}
+	}
+
+	/*---------------------------------------------------------------------------*/
 	// TEXT REDUCER
     // Replace "?" with your own input key / value types, i.e., the output
     // key / value types of your mapper function
 
-    public static class TextReducer extends Reducer<?, ?, Text, Text> {
-        private final static Text emptyText = new Text("");
+	public static class TextReducer extends Reducer<Text, MapWritable, Text, Text> {
+		private final static Text emptyText = new Text("");
 
-        public void reduce(Text key, Iterable<Tuple> queryTuples, Context context)
-            throws IOException, InterruptedException
-        {
-            // Implementation of you reducer function
+		public void reduce(Text key, Iterable<MapWritable> wordmaps, Context context)
+		throws IOException, InterruptedException
+		{
+      // Implementation of your reducer function
+      private TreeMap<String, IntWritable> map = new TreeMap<>();
 
-            // Write out the results; you may change the following example
-            // code to fit with your reducer function.
-            //   Write out the current context key
-            context.write(key, emptyText);
-            //   Write out query words and their count
-            for(String queryWord: map.keySet()){
-                String count = map.get(queryWord).toString() + ">";
-                queryWordText.set("<" + queryWord + ",");
-                context.write(queryWordText, new Text(count));
-            }
-            //   Empty line for ending the current context key
-            context.write(emptyText, emptyText);
-        }
-    }
+			for(MapWritable wordmap : wordmaps){
+				for(Map.Entry<Text, IntWritable> entry: wordmap.entrySet()){
+					String query = entry.getKey().toString();
+					IntWritable count = entry.getValue();
+					wordSet.put(query, count);
+      	}
+			}
 
-/*---------------------------------------------------------------------------*/
+      // Write out the results; you may change the following example
+      // code to fit with your reducer function.
+      //   Write out the current context key
+			context.write(key, emptyText);
+      //   Write out query words and their count
+			for(String queryWord: map.keySet()){
+				String count = map.get(queryWord).toString() + ">";
+				queryWordText.set("<" + queryWord + ",");
+				context.write(queryWordText, new Text(count));
+			}
+      //   Empty line for ending the current context key
+			context.write(emptyText, emptyText);
+		}
+	}
 
-    public int run(String[] args) throws Exception {
-        Configuration conf = this.getConf();
+	/*---------------------------------------------------------------------------*/
+
+	public int run(String[] args) throws Exception {
+		Configuration conf = this.getConf();
 
         // Create job
         Job job = new Job(conf, "am74874_dpv292"); // Replace with your EIDs
@@ -114,19 +182,16 @@ public class TextAnalyzer extends Configured implements Tool {
 
         // Execute job and return status
         return job.waitForCompletion(true) ? 0 : 1;
-    }
+      }
 
     // Do not modify the main method
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new TextAnalyzer(), args);
-        System.exit(res);
-    }
+      public static void main(String[] args) throws Exception {
+      	int res = ToolRunner.run(new Configuration(), new TextAnalyzer(), args);
+      	System.exit(res);
+      }
 
-    // You may define sub-classes here. Example:
-    // public static class MyClass {
-    //
-    // }
-}
+      /*---------------------------------------------------------------------------*/
 
+// You may define sub-classes here.
 
 
